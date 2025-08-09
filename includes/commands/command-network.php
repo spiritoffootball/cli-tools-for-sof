06964c035cd7d02de7170560286a06c4fae3d4c3
Christian Wach <needle@haystack.co.uk>
2025-08-09 14:03:29 +0100

    Add utility to remove BBPress roles

--- includes/commands/command-network.php
+++ includes/commands/command-network.php
@@ -27,6 +27,101 @@
 class CLI_Tools_SOF_Command_Network extends CLI_Tools_SOF_Command {
 
 	/**
+	 * Delete the roles that BBPress added to the Network.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--name=<name>]
+	 * : Specify the name of role to remove.
+	 *
+	 * [--all]
+	 * : Run the command for all roles.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *       # Remove the roles that BBPress added to the Network.
+	 *       $ wp sof network role-delete --url=https://spirit-of-germany.cmw --name=bbp_keymaster
+	 *       Deleting role bbp_keymaster on site https://spirit-of-germany.cmw
+	 *       Success: Role deleted.
+	 *
+	 *       # Remove the roles that BBPress added to the Network.
+	 *       $ wp sof network role-delete --url=https://spirit-of-germany.cmw
+	 *       Deleting role bbp_keymaster on site https://spirit-of-germany.cmw
+	 *       Deleting role bbp_spectator on site https://spirit-of-germany.cmw
+	 *       Deleting role bbp_blocked on site https://spirit-of-germany.cmw
+	 *       Deleting role bbp_moderator on site https://spirit-of-germany.cmw
+	 *       Deleting role bbp_participant on site https://spirit-of-germany.cmw
+	 *       Success: All roles deleted.
+	 *
+	 * @subcommand role-delete
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $args The WP-CLI positional arguments.
+	 * @param array $assoc_args The WP-CLI associative arguments.
+	 */
+	public function role_delete( $args, $assoc_args ) {
+
+		// Grab associative arguments.
+		$all  = (bool) \WP_CLI\Utils\get_flag_value( $assoc_args, 'all', false );
+		$name = (string) \WP_CLI\Utils\get_flag_value( $assoc_args, 'name', '' );
+
+		// Define BBPress Roles.
+		$roles = [
+			'bbp_keymaster',
+			'bbp_spectator',
+			'bbp_blocked',
+			'bbp_moderator',
+			'bbp_participant',
+		];
+
+		// Grab URL from config.
+		$url = '';
+		$wp_cli_config = WP_CLI::get_config();
+		if ( ! empty( $wp_cli_config['url'] ) ) {
+			$url = $wp_cli_config['url'];
+		}
+
+		// Maybe process all Roles.
+		if ( ! empty( $all ) ) {
+
+			// Delete all Roles.
+			foreach ( $roles as $role ) {
+				WP_CLI::log( sprintf( WP_CLI::colorize( '%GDeleting role %Y%s%n on site%n %Y%s%n' ), $role, $url ) );
+				$this->role_delete_from_users( $role );
+				remove_role( $role );
+			}
+
+			WP_CLI::success( 'All roles deleted.' );
+
+		} else {
+
+			// Show feedback.
+			WP_CLI::log( sprintf( WP_CLI::colorize( '%GDeleting role %Y%s%n on site%n %Y%s%n' ), $name, $url ) );
+
+			// Bail if not a BBPress role.
+			if ( empty( $name ) ) {
+				WP_CLI::error( 'You must specify a role or use the "--all" argument.' );
+			}
+
+			// Bail if not a BBPress role.
+			if ( ! in_array( $name, $roles ) ) {
+				WP_CLI::error( sprintf( WP_CLI::colorize( 'Unknown role: %Y%s%n' ), $name ) );
+			}
+
+			// Remove from Users first.
+			$this->role_delete_from_users( $name );
+
+			// Now remove the Role.
+			remove_role( $name );
+
+			WP_CLI::success( 'Role deleted.' );
+
+		}
+
+	}
+
+	/**
 	 * Delete spam comments and JetPack Contact Form Submissions.
 	 *
 	 * Use the `--all` flag with caution - it has crashed our server in the past. It is,
@@ -123,6 +218,30 @@ class CLI_Tools_SOF_Command_Network extends CLI_Tools_SOF_Command {
 				WP_CLI::error( sprintf( WP_CLI::colorize( 'Unknown type: %Y%s%n' ), $type ) );
 			}
 
+		}
+
+	}
+
+	/**
+	 * Deletes spam Comments for a given Site URL.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $name The name of the Role.
+	 */
+	private function role_delete_from_users( $name = '' ) {
+
+		// Build query to get all Users that have the role.
+		$args = [
+			'role' => $name,
+		];
+
+		// Get the Users.
+		$users = get_users( $args );
+
+		// Remove the role from any Users that have it.
+		foreach ( $users as $user ) {
+			$user->remove_role( $name );
 		}
 
 	}
